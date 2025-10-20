cdf2f1855fa29bcb7432275bb54a80d02b04ca73
Maxim Perman <maxim@7perman.ru>
2025-10-20 19:47:42 +0700

    Исправлен скрипт

---
+++ Module_1/Homework_4/Task_5/bin/setup_backup.sh
@@ -0,0 +1,4 @@
+#!/bin/ash
+
+now=$(/bin/date +"%s_%Y-%m-%d")
+/usr/bin/mysqldump --opt -h ${MYSQL_HOST} -u ${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} > "/backup/${now}_${MYSQL_DATABASE}.sql"
