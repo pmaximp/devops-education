f083813c7ed37333a1d1d09c78f446c6f9e74fae
Maxim Perman <maxim@7perman.ru>
2025-11-06 20:48:51 +0700

    Fix data

--- Module_2/Homework_3/Task_3/locals.tf
+++
@@ -1,3 +0,0 @@
-locals {
-  ssh_public_key = file("~/.ssh/id_rsa.pub")
-}
--- Module_2/Homework_3/Task_4/ansible.tf
+++
@@ -1,9 +0,0 @@
-resource "local_file" "ansible_inventory" {
-  content = templatefile("${path.module}/ansible.tftpl", 
-  {
-    webservers = yandex_compute_instance.vms_web_count
-    database = yandex_compute_instance.vms_db_each
-    storage = yandex_compute_instance.vm_storage
-  })
-  filename = "${path.module}/hosts.ini"
-}
